e2fc1bf5e1d9f17c7d4afeaf29d9bbeced8d93c5
Abdulhafiz Gazali <ahgazali108@gmail.com>
2023-11-08 14:17:00 -0800

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,13 +7,13 @@ import requests
 def number_of_subscribers(subreddit):
     '''Retrieves the number of subscribers in a given subreddit.
     '''
-   url = f"https://www.reddit.com/r/{subreddit}/about.json"
-   headers = {'User-Agent': 'My user Agent 1.0'}
-   response = requests.get(url, headers=headers, allow_redirects=False)
+    url = "https://www.reddit.com/r/" + subreddit + "/about.json"
+    headers = {'User-Agent': 'My user Agent 1.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
-   if response.status_code == 200:
-      data = response.json().get('data', {})
-      sub_count = data.get('subscribers', 0)
-      return sub_count
-   else:
-      return 0
+    if response.status_code == 200:
+       data = response.json().get('data', {})
+       sub_count = data.get('subscribers', 0)
+       return sub_count
+    else:
+        return 0
